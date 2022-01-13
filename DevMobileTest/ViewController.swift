27244bdf60331ddc03dd7a4326241f1f3ac6d74f
Ilham Cahyahadi <ilham.cahyahadi@aksesciptasolusi.com>
2022-01-13 12:31:15 +0700

    Initial Commit

---
+++ DevMobileTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DevMobileTest
+//
+//  Created by acs macbookpro on 13/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
